ae78aa1cf8becad2cf2d4b6649bb9a93ae77a65f
Ionut Medrea <ionutmedrea@yahoo.com>
2020-06-19 17:08:10 +0300

    add si delete functionale + tranzitie intre login si search

--- proiectfis/src/main/java/Controllers/LoginController.java
+++ proiectfis/src/main/java/Controllers/LoginController.java
@@ -1,22 +1,33 @@
 package Controllers;
 
-import Exceptions.UsernameAlreadyExistsException;
 import Exceptions.UsernameOrPasswordIncorrect;
-import Services.UserService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import sun.security.tools.keytool.Main;
+
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 import static Services.UserService.encodePassword;
 import static Services.UserService.verifyLogin;
 
 
-public class LoginController {
+public class LoginController implements Initializable {
 
+    private Scene secondScene;
     @FXML
     private Text loginMessage;
     @FXML
@@ -26,19 +37,49 @@ public class LoginController {
     @FXML
     private ChoiceBox<String> role;
 
-    @FXML
-    public void initialize() {
+    @FXML @Override
+    public void initialize(URL location, ResourceBundle resources) {
         role.getItems().addAll("Client", "Admin");
     }
 
+    Stage stage;
+    Parent root;
+    private boolean isActive;
     @FXML
-    public void handleLoginAction() {
+    public void handleLoginAction(ActionEvent event) throws Exception {
         try {
-            if(verifyLogin(usernameField.getText(), encodePassword(usernameField.getText(),passwordField.getText()), role.getValue()))
+            if(verifyLogin(usernameField.getText(), encodePassword(usernameField.getText(),passwordField.getText()), role.getValue())) {
                 loginMessage.setText("Login successfully!");
+                isActive=true;
+            }
             else throw new UsernameOrPasswordIncorrect();
         } catch (UsernameOrPasswordIncorrect e) {
             loginMessage.setText(e.getMessage());
         }
     }
+    public void changeScreenButtonPushed() throws IOException {
+        if(isActive==true) {
+            Parent search = FXMLLoader.load(getClass().getClassLoader().getResource("search.fxml"));
+            Scene newScene = new Scene(search);
+            Stage window = new Stage();
+            window.setScene(newScene);
+            window.show();
+        }
+    }
+
+/*
+    private void secondStage(Stage Stage) throws IOException {
+        UserService.loadUsersFromFile();
+        ComponentService.loadCompsFromFile();
+        Parent search = FXMLLoader.load(getClass().getResource("search.fxml"));
+        Stage.setTitle("Componente PC");
+        stage.setFullScreen(Boolean.parseBoolean("True"));
+        //Stage.setScene(new Scene(search, 1200, 375));
+        //Stage.setMinHeight(400);
+        //Stage.setMinWidth(600);
+        Stage.show();
+    }
+
+
+ */
 }
--- proiectfis/src/main/java/Main.java
+++ proiectfis/src/main/java/Main.java
@@ -6,32 +6,42 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.util.Objects;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
- /*   @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
-        //Scene scene = new Scene(root);
-        primaryStage.setTitle("Registration ");
-        primaryStage.setScene(new Scene(root, 720, 380));
-        primaryStage.show();
-    }
-  */
+/*
  @Override
- public void start(Stage primaryStage) throws Exception{
+ public void start(Stage primaryStage) throws Exception {
      UserService.loadUsersFromFile();
      ComponentService.loadCompsFromFile();
 
-     Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("search.fxml")));
+     Parent login = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
+     //Parent search = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("search.fxml")));
      primaryStage.setTitle("Componente PC");
-     primaryStage.setScene(new Scene(root, 1200, 375));
+     primaryStage.setScene(new Scene(login, 1200, 375));
      primaryStage.setMinHeight(400);
      primaryStage.setMinWidth(600);
      primaryStage.show();
-    }
+ }
 
+ */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        UserService.loadUsersFromFile();
+        ComponentService.loadCompsFromFile();
+        start1(primaryStage, "login.fxml");
+    }
+    public void start1(Stage primaryStage, String screen) throws Exception {
+
+        Parent a = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource(screen)));
+        primaryStage.setTitle("Componente PC");
+        primaryStage.setScene(new Scene(a, 1200, 375));
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(600);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
